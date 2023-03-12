c062d50a911065712dca43fc1230d975cbb1ed3d
bedirhandemirci <127402571+bedirhandemirci@users.noreply.github.com>
2023-03-12 17:38:27 +0300

    Create clientdatabase.js

---
+++ clientdatabase.js
@@ -0,0 +1,52 @@
+const defaultCards = {
+  card1: {
+    cardNumber: '5555555555554444',
+    expirationDate: '12/23',
+    cvv: '678',
+    cardholderName: 'Jane Doe'
+  },
+  card2: {
+    cardNumber: '4111111111111111',
+    expirationDate: '03/25',
+    cvv: '456',
+    cardholderName: 'Bob Smith'
+  },
+  card3: {
+    cardNumber: '378282246310005',
+    expirationDate: '06/24',
+    cvv: '789',
+    cardholderName: 'Alice Johnson'
+  },
+  card4: {
+    cardNumber: '5105105105105100',
+    expirationDate: '09/22',
+    cvv: '123',
+    cardholderName: 'Chris Brown'
+  },
+  card5: {
+    cardNumber: '4222222222222',
+    expirationDate: '11/23',
+    cvv: '456',
+    cardholderName: 'Lisa Williams'
+  },
+  card6: {
+    cardNumber: '6011111111111117',
+    expirationDate: '05/24',
+    cvv: '123',
+    cardholderName: 'John Smith'
+  },
+  card7: {
+    cardNumber: '3530111333300000',
+    expirationDate: '04/25',
+    cvv: '345',
+    cardholderName: 'Sarah Johnson'
+  },
+  card8: {
+    cardNumber: '5555666677778884',
+    expirationDate: '08/23',
+    cvv: '789',
+    cardholderName: 'Michael Lee'
+  }
+};
+
+export default defaultCards;
